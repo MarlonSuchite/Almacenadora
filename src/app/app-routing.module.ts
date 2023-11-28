40b1111cb36350e167525fc48ae50174daa02946
MarlonSuchite <marlon1234suchite@gmail.com>
2023-11-27 22:53:13 -0600

    Se implemento el patron de diseño Lazi loading para la carga diferida de los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'security',
+    loadChildren: () =>
+      import('./security/security.module').then(m => m.SecurityModule)
+  },
+  {
+    path: 'shared',
+    loadChildren: () =>
+      import('./shared/shared.module').then(m => m.SharedModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
