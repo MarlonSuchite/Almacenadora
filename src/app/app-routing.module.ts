fefdf0f573df087baa288cb336fb3ff09d4bf188
MarlonSuchite <marlon1234suchite@gmail.com>
2023-11-28 15:56:51 -0600

    Se incorporo bootstrap al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    redirectTo: '',
+    pathMatch: 'full'
+  },
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
