2cf073ed6fc01c8b4abb5691a588c268ecb6ef72
MarlonSuchite <marlon1234suchite@gmail.com>
2023-12-20 01:17:27 -0600

    Se realizo componente de pie de pagina en modulo shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, FooterComponent],
   imports: [CommonModule, SharedRoutingModule, RouterModule],
-  exports: [NavBarComponent]
+  exports: [NavBarComponent, FooterComponent]
 })
 export class SharedModule {}
