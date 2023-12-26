ab43a9c6140cb3dc97ba7d5b73094902a8944ffd
MarlonSuchite <marlon1234suchite@gmail.com>
2023-12-26 00:10:49 -0600

    Se realizo la parte visual del componente sucursales

--- src/app/branches/Components/branches/branches.component.ts
+++ src/app/branches/Components/branches/branches.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './branches.component.html',
   styleUrls: ['./branches.component.scss']
 })
-export class BranchesComponent {}
+export class BranchesComponent {
+  sucursales: any[] = [
+    { name: 'Ander', address: 'Colonia los anded', status: true, storage: 5 },
+    { name: 'Celasa', address: 'Colonia los anded', status: false, storage: 5 },
+    { name: 'USA', address: 'Colonia los anded', status: true, storage: 5 },
+    { name: 'FICOSA', address: 'Colonia los anded', status: false, storage: 5 }
+  ];
+}
