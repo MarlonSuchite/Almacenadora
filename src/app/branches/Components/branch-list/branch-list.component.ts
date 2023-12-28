42c55228686e2371b219efb262e3e9a23b6bfb4a
MarlonSuchite <marlon1234suchite@gmail.com>
2023-12-27 23:16:17 -0600

    Se creo componente branch-list

---
+++ src/app/branches/Components/branch-list/branch-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branch-list',
+  templateUrl: './branch-list.component.html',
+  styleUrls: ['./branch-list.component.scss']
+})
+export class BranchListComponent {}
--- src/app/branches/branches.module.ts
+++ src/app/branches/branches.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BranchesRoutingModule } from './branches-routing.module';
 import { BranchesComponent } from './Components/branches/branches.component';
 import { BranchComponent } from './Components/branch/branch.component';
+import { BranchListComponent } from './Components/branch-list/branch-list.component';
 
 @NgModule({
-  declarations: [BranchesComponent, BranchComponent],
+  declarations: [BranchesComponent, BranchComponent, BranchListComponent],
   imports: [CommonModule, BranchesRoutingModule]
 })
 export class BranchesModule {}
