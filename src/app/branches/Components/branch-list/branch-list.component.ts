d9d70559ecce6e2d9962e7d224824a01049c1d99
MarlonSuchite <marlon1234suchite@gmail.com>
2024-01-07 00:13:00 -0600

    Se inyecto el servicio de branches en branch-list

--- src/app/branches/Components/branch-list/branch-list.component.ts
+++ src/app/branches/Components/branch-list/branch-list.component.ts
@@ -1,49 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { BranchService } from '../../Service/branch.service';
 
 @Component({
   selector: 'app-branch-list',
   templateUrl: './branch-list.component.html',
   styleUrls: ['./branch-list.component.scss']
 })
-export class BranchListComponent {
-  sucursales: any[] = [
-    {
-      id: 736457356312312,
-      name: 'Ander',
-      address: 'Colonia los anded',
-      status: true,
-      storage: 5
-    },
-    {
-      id: 834524312312312,
-      name: 'Celasa',
-      address: 'Colonia los anded',
-      status: false,
-      storage: 5
-    },
-    {
-      id: 123123123134524,
-      name: 'USA',
-      address: 'Colonia los anded',
-      status: true,
-      storage: 5
-    },
-    {
-      id: 643123123124324,
-      name: 'FICOSA',
-      address: 'Colonia los anded',
-      status: false,
-      storage: 5
-    }
-  ];
+export class BranchListComponent implements OnInit {
+  sucursales!: any[];
 
   constructor(
     private router: Router,
-    private activadteRoute: ActivatedRoute
+    private activadteRoute: ActivatedRoute,
+    private _branchService: BranchService
   ) {}
 
-  goBranch() {
+  ngOnInit(): void {
+    this.sucursales = this._branchService.getBranches();
+  }
+
+  goBranch(): void {
     this.router.navigate(['/branches/branch']);
   }
 
--- src/app/branches/Service/branch.service.ts
+++ src/app/branches/Service/branch.service.ts
@@ -4,7 +4,42 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BranchService {
+  sucursales: any[] = [
+    {
+      id: 736457356312312,
+      name: 'Ander',
+      address: 'Colonia los anded',
+      status: true,
+      storage: 5
+    },
+    {
+      id: 834524312312312,
+      name: 'Celasa',
+      address: 'Colonia los anded',
+      status: false,
+      storage: 5
+    },
+    {
+      id: 123123123134524,
+      name: 'USA',
+      address: 'Colonia los anded',
+      status: true,
+      storage: 5
+    },
+    {
+      id: 643123123124324,
+      name: 'FICOSA',
+      address: 'Colonia los anded',
+      status: false,
+      storage: 5
+    }
+  ];
+
   constructor() {
     //Constructor
+  }
+
+  getBranches() {
+    return this.sucursales;
   }
 }
